7a8415decb72794f4c696b0ec986b544cbfca383
bismarck <bismarquemomanyi@gmail.com>
2023-01-19 05:28:54 -0500

    push

---
+++ swap.c
@@ -0,0 +1,20 @@
+include "monty.h"
+
+/**
+* swap - swaps the value of the top two elements to the stack
+* @stack: pointer that point to stack
+* @line_number: line number of instruction
+**/
+void swap(stack_t **stack, unsigned int line_number)
+{
+	int temp;
+
+	if (len(stack) < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	temp = (*stack)->n;
+	(*stack)->n = (*stack)->next->n;
+	(*stack)->next->n = temp;
+}
